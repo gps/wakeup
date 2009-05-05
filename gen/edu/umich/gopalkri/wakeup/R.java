16716a3f8773a986f092c0190eafe89919cb91eb
Gopalkrishna Sharma <gopalkri@umich.edu>
2009-05-04 22:55:31 -0400

    Created Home Activity's UI

--- gen/edu/umich/gopalkri/wakeup/R.java
+++ gen/edu/umich/gopalkri/wakeup/R.java
@@ -13,11 +13,22 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int home_create_new_alarm=0x7f050002;
+        public static final int home_done=0x7f050001;
+        public static final int home_existing_alarms_spinner=0x7f050000;
+        public static final int home_manage_existing_alarms=0x7f050003;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int home=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int home_create_new_alarm=0x7f040005;
+        public static final int home_done=0x7f040004;
+        public static final int home_existing_alarm_spinner_prompt=0x7f040003;
+        public static final int home_manage_existing_alarms=0x7f040006;
+        public static final int home_set_existing_alarm=0x7f040002;
+        public static final int home_title=0x7f040001;
     }
 }
--- src/edu/umich/gopalkri/wakeup/Home.java
+++ src/edu/umich/gopalkri/wakeup/Home.java
@@ -3,11 +3,13 @@ package edu.umich.gopalkri.wakeup;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Home extends Activity {
+public class Home extends Activity
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.home);
     }
 }
