3f3a362673719e085ebb1ea6d186b53a9367be10
Gopalkrishna Sharma <gopalkri@umich.edu>
2009-05-04 23:29:30 -0400

    Wrote Home Activity with stubs for event handlers.

--- gen/edu/umich/gopalkri/wakeup/R.java
+++ gen/edu/umich/gopalkri/wakeup/R.java
@@ -14,10 +14,12 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int home_create_new_alarm=0x7f050002;
-        public static final int home_done=0x7f050001;
-        public static final int home_existing_alarms_spinner=0x7f050000;
+        public static final int home_alarms_exist_layout=0x7f050000;
+        public static final int home_create_new_alarm=0x7f050005;
+        public static final int home_done=0x7f050002;
+        public static final int home_existing_alarms_spinner=0x7f050001;
         public static final int home_manage_existing_alarms=0x7f050003;
+        public static final int home_no_alarms_exist=0x7f050004;
     }
     public static final class layout {
         public static final int home=0x7f030000;
@@ -28,6 +30,7 @@ public final class R {
         public static final int home_done=0x7f040004;
         public static final int home_existing_alarm_spinner_prompt=0x7f040003;
         public static final int home_manage_existing_alarms=0x7f040006;
+        public static final int home_no_alarms_exist=0x7f040007;
         public static final int home_set_existing_alarm=0x7f040002;
         public static final int home_title=0x7f040001;
     }
--- src/edu/umich/gopalkri/wakeup/Home.java
+++ src/edu/umich/gopalkri/wakeup/Home.java
@@ -2,7 +2,18 @@ package edu.umich.gopalkri.wakeup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+/**
+ * The Activity that represents the "Home" screen.
+ *
+ * @author Gopalkrishna Sharma (gopalkri@umich.edu)
+ */
 public class Home extends Activity
 {
     /** Called when the activity is first created. */
@@ -11,5 +22,72 @@ public class Home extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        setupUI();
+    }
+
+    /**
+     * Sets up the UI for this activity. Contains all the event handling code
+     * required.
+     */
+    private void setupUI()
+    {
+        // Handle the onClick event for the Done button.
+        Button done = (Button) findViewById(R.id.home_done);
+        done.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {}
+        });
+
+        // Handle the onClick event for the Create New Alarm button.
+        Button createNewAlarm = (Button) findViewById(R.id.home_create_new_alarm);
+        createNewAlarm.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {}
+        });
+
+        // Handle the onClick event for the Manage Existing Alarms button.
+        Button manageExistingAlarms = (Button) findViewById(R.id.home_manage_existing_alarms);
+        manageExistingAlarms.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {}
+        });
+
+        // Setup the Existing Alarms spinner.
+        Spinner existingAlarms = (Spinner) findViewById(R.id.home_existing_alarms_spinner);
+
+        LinearLayout alarmsExistLayout = (LinearLayout) findViewById(R.id.home_alarms_exist_layout);
+        TextView noAlarmsExist = (TextView) findViewById(R.id.home_no_alarms_exist);
+        String[] alarmNames = getAllAlarmNames();
+        // If there are existing alarms, the layout that has UI elements to set/manage existing
+        // alarms must be visible and enabled. If not, they must be disabled.
+        if (alarmNames != null)
+        {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, alarmNames);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            existingAlarms.setAdapter(adapter);
+
+            alarmsExistLayout.setEnabled(true);
+            alarmsExistLayout.setVisibility(View.VISIBLE);
+            noAlarmsExist.setEnabled(false);
+            noAlarmsExist.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            alarmsExistLayout.setEnabled(false);
+            alarmsExistLayout.setVisibility(View.INVISIBLE);
+            noAlarmsExist.setEnabled(true);
+            noAlarmsExist.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private String[] getAllAlarmNames()
+    {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
