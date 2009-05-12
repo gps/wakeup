dd56430bc90a5b9fc2e8c296d48b857907c302a9
Gopalkrishna Sharma <gopalkri@umich.edu>
2009-05-12 02:57:09 -0400

    Added screenshots and fixed LGPL statement in source files.

--- removelgpl.py
+++ removelgpl.py
@@ -6,18 +6,18 @@ Email: gopalkri@umich.edu / gopalkrishnaps@gmail.com
 
 This file is part of WakeUp!.
 
-Foobar is free software: you can redistribute it and/or modify
+Wake Up! is free software: you can redistribute it and/or modify
 it under the terms of the Lesser GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful,
+Wake Up! is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 Lesser GNU General Public License for more details.
 
 You should have received a copy of the Lesser GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with Wake Up!.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 """
@@ -28,18 +28,18 @@ Email: gopalkri@umich.edu / gopalkrishnaps@gmail.com
 
 This file is part of WakeUp!.
 
-Foobar is free software: you can redistribute it and/or modify
+Wake Up! is free software: you can redistribute it and/or modify
 it under the terms of the Lesser GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Foobar is distributed in the hope that it will be useful,
+Wake Up! is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 Lesser GNU General Public License for more details.
 
 You should have received a copy of the Lesser GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with Wake Up!.  If not, see <http://www.gnu.org/licenses/>.
 -->
 """
 
